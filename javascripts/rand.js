04054e0ae051c50f532fdc29f8c20a30566f71bd
Randolph Voorhies <rand@ilab21.usc.edu>
2012-01-31 18:07:01 -0800

    Added cool scrolling to the nav links

--- javascripts/rand.js
+++ javascripts/rand.js
@@ -3,8 +3,19 @@ var rightarrow='\u25B6';
 var downarrow='\u25BC';
 
 $(document).ready(function() {
-  $(".skilldescription").hide();
 
+  //------------------------------ Setup the navigation links
+  // Let's do a cute slow scroll so that users know they're just sliding down the page, rather than jumping to a new one
+  $("nav li a").each(function(index) {
+    $(this).click(function()
+      {
+        $('html,body').animate({scrollTop:$($(this).attr('href')).offset().top}, 'fast'); return false;
+      })
+    });
+
+
+  //------------------------------ Setup the skill description bars
+  $(".skilldescription").hide();
 	$(".skillbar").each(function(index) {
 		$(this).prepend('<span class="skillarrow">'+rightarrow+'</span> ');
 
@@ -16,7 +27,6 @@ $(document).ready(function() {
       );
 
 		$(this).click(function() {
-
 
       if(arrow.text().trim().charAt(0) == downarrow)
       {
